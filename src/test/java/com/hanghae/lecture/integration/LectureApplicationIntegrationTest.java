4963c0130eb1d585633513396534dff4114f1d58
Kimseonkyoung <kwswaver@gmail.com>
2024-12-27 08:39:48 +0900

    Test: 통합테스트 클래스명 수정 및 yml, sql 파일 일부 수정
    - schema 가시성을 위한 들여쓰기 변경
    - .yml ddl 옵션 (create -> create-drop)

--- src/test/java/com/hanghae/lecture/integration/LectureApplicationIntegrationTest.java
+++ src/test/java/com/hanghae/lecture/integration/LectureApplicationIntegrationTest.java
@@ -6,7 +6,6 @@ import com.hanghae.lecture.domain.Lecture;
 import com.hanghae.lecture.domain.LectureRegistration;
 import com.hanghae.lecture.domain.LectureRepository;
 import com.hanghae.lecture.domain.exception.DuplicateRegistrationException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-class LectureApplicationServiceTest {
+class LectureApplicationIntergrationTest {
 
     @Autowired
     private LectureRegistrationService lectureRegistrationService;
